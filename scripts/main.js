b3395c04437464e9694960b053f8b8c473b2cf69
DphnZwp <zwuupdaphne@gmail.com>
2022-04-10 22:06:57 +0200

    Error state

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,52 @@
-// Empty state
-const display = document.querySelector('.empty-state')
-display.style.display = 'none';
+// Business card api
+const apiBase = 'https://tribe.api.fdnd.nl/v1/member'
+const businessCardData = document.querySelector('.business-card');
 // Loading state
-const preLoaderWrapper = document.querySelector('.preloader-wrapper')
-preLoaderWrapper.style.display = "block"
-// Business card ids
-const nameEl = document.getElementById('name')
-const frontendTitle = document.getElementById('frontend-title')
-const githubEl = document.getElementById("github")
-const bioEl = document.getElementById('bio')
-const avatar = document.getElementById('avatar')
-const imagePig = document.querySelector('.image-pig')
-imagePig.src = 'assets/daphne-pig.png'
-const businessCard = document.querySelector('.business-card')
-businessCard.style.opacity = '0';
+const loadingState = document.querySelector('.loading-state')
+loadingState.classList.add('loading-show')
+// Empty state
+const emptyState = document.querySelector('.empty-state')
+emptyState.classList.add('empty-dissapear')
+// Error state
+const errorState = document.querySelector('.error-state')
 
-fetch('https://tribe.api.fdnd.nl/v1/member')
-.then(response=> {
-	preLoaderWrapper.style.display = "none"
-	businessCard.style.opacity = '100%';
-	return response.json()
-})
-.then (json => {
-	let data = json.data[5];
+renderMembers()
 
-	nameEl.innerText = `${data.name} ${data.surname}`
-	frontendTitle.innerText = `Frontend Developer`
-	githubEl.innerText = `${data.githubHandle}`
-	bioEl.innerText = `${data.bio}`
-	avatar.src = `${data.avatar}`
-});
+async function getMembers() {
+		try {
+			const response = await fetch(apiBase)
+			const data = await response.json()
+    	return data.data
+		} catch (error) {
+			errorState.classList.add('error-show')
+		}
+    
+}
+
+async function renderMembers() {
+  const members = await getMembers()
+	// Hide loading
+	loadingState.classList.remove('loading-show')
+	// Filter and render members
+      members.filter((member) => {
+				return member.memberId === 13
+			})
+			.map((member) => {
+        // Create a HTML business card
+        businessCardData.insertAdjacentHTML('beforeend',
+            `
+						<section class="information">
+							<h2>${member.name} ${member.prefix} ${member.surname}</h2>
+							<h3>Frontend Developer</h3>
+							<a href="${member.githubHandle}" target="_blank">${member.githubHandle}</a>
+							<h3>Bio</h3>
+							<p>${member.bio}</p>
+							<img class="image-pig" src="assets/daphne-pig.png">
+						</section>
+						<section class="illustration">
+							<img class="image-avatar" src="${member.avatar}" alt="">
+						</section>
+        `
+        )
+    })
+}
